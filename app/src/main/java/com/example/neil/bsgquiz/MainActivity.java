df12db2e8ff939c202333b1218138b57175ad1f1
Neil <nruggier@gmail.com>
2016-11-16 15:05:24 -0500

    Fix resource handling bug.  String[] now load properly

--- app/src/main/java/com/example/neil/bsgquiz/MainActivity.java
+++ app/src/main/java/com/example/neil/bsgquiz/MainActivity.java
@@ -1,31 +1,75 @@
 package com.example.neil.bsgquiz;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] questionHeaders, answerKey, questions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        questionHeaders = res.getStringArray(R.array.question_header_array);
+        answerKey = res.getStringArray(R.array.answer_key_array);
+        questions = res.getStringArray(R.array.questions_array);
+    }
+
+    int correctAnswers = 0;
+    int questionCounter = 0;
+
+
+    public void submitAnswer() {
+        //create Edit Text object, and extract user input answer from it
+        EditText userAnswerEditText = (EditText) findViewById(R.id.answer_text);
+        String userAnswer = userAnswerEditText.getText().toString();
+
+        //compare against answer key and tell user if right or wrong
+        if (userAnswer.compareToIgnoreCase(answerKey[questionCounter]) == 0) {
+            Toast.makeText(this, "That's Correct!", Toast.LENGTH_SHORT).show();
+            correctAnswers++;
+        } else {
+            Toast.makeText(this, "Sorry, That's Incorrect", Toast.LENGTH_SHORT).show();
+        }
+        nextQuestion();
     }
 
     /**
-     *
-     * @param answer: User input answer to corresponding question
-     * @return boolean correct or incorrect
-     */
-    private boolean submitAnswer(String answer) {
-        //TODO compare user's answer against correct answer
-    }
-
-    /**
-     *
      * @param questionNumber: what question are we getting a hint for
      * @return String containing a hint to give to the user
      */
     private String getHint(int questionNumber) {
+        return "0";
+    }
 
+    private void nextQuestion() {
+        //step forward question header
+        TextView headerText = (TextView) findViewById(R.id.question_header);
+        headerText.setText(questionHeaders[questionCounter]);
+
+        //step forward question text
+        TextView questionText = (TextView) findViewById(R.id.question_text_view);
+        questionText.setText(questions[questionCounter]);
+
+        //reset hint for EditText
+        EditText userAnswerEditText = (EditText) findViewById(R.id.answer_text);
+        userAnswerEditText.setHint("Answer");
+
+        //update correct answer counter
+        TextView correctAnswers = (TextView) findViewById(R.id.correct_answer_counter);
+        correctAnswers.setText(correctAnswers + " / 8");
+
+        questionCounter++;
     }
 }
